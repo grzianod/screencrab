d3d66fca6cfa62eb2067c05bfce6c3236d5c6c62
grzianod <grazianodinocca@icloud.com>
2023-08-07 19:50:03 +0200

    added global shortcuts

--- screencrab/src-tauri/src/menu.rs
+++ screencrab/src-tauri/src/menu.rs
@@ -17,16 +17,16 @@ pub fn create_context_menu() -> Menu {
                               Menu::new().add_item(CustomMenuItem::new("capture_fullscreen".to_string(), "Fullscreen Capture").accelerator("CmdOrCtrl+F"))
                                   .add_item(CustomMenuItem::new("capture_custom".to_string(), "Custom Capture").accelerator("CmdOrCtrl+C"))
                                   .add_native_item(MenuItem::Separator)
-                                  .add_item(CustomMenuItem::new("capture_mouse_pointer".to_string(), "Toggle Capture Mouse Pointer").accelerator("Option+M"))
-                                  .add_item(CustomMenuItem::new("copy_to_clipboard".to_string(), "Toggle Copy To Clipboard").accelerator("Option+C"))
-                                  .add_item(CustomMenuItem::new("open".to_string(), "Toggle Edit After Capture").accelerator("Option+E")));
+                                  .add_item(CustomMenuItem::new("capture_mouse_pointer".to_string(), "Capture Mouse Pointer").accelerator("Option+M"))
+                                  .add_item(CustomMenuItem::new("copy_to_clipboard".to_string(), "Copy To Clipboard").accelerator("Option+C"))
+                                  .add_item(CustomMenuItem::new("open".to_string(), "Edit After Capture").accelerator("Option+E")));
     let record = Submenu::new("Record",
                               Menu::new().add_item(CustomMenuItem::new("record_fullscreen".to_string(), "Fullscreen Record").accelerator("CmdOrCtrl+Option+F"))
                                   .add_item(CustomMenuItem::new("record_custom".to_string(), "Custom Record").accelerator("CmdOrCtrl+Option+C"))
                                   .add_item(CustomMenuItem::new("stop_record".to_string(), "Stop Recording").accelerator("CmdOrCtrl+Option+S").disabled())
                                   .add_native_item(MenuItem::Separator)
-                                  .add_item(CustomMenuItem::new("capture_mouse_pointer".to_string(), "Toggle Record Mouse Pointer").accelerator("Option+M"))
-                                  .add_item(CustomMenuItem::new("open".to_string(), "Toggle Open After Record").accelerator("Option+O")));
+                                  .add_item(CustomMenuItem::new("capture_mouse_pointer".to_string(), "Record Mouse Pointer").accelerator("Option+M"))
+                                  .add_item(CustomMenuItem::new("open".to_string(), "Open After Record").accelerator("Option+O")));
     let help = Submenu::new("Help",
                             Menu::new().add_item(CustomMenuItem::new("learn_more", "Learn More")));
 
