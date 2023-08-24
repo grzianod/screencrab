56f900d2f4c3adfe5ebbea9eca821030a4351d85
Antonio Ferrigno <s316467@studenti.polito.it>
2023-08-24 17:10:37 +0200

    Fixed getting only part of the screen

--- screencrab/src-tauri/src/screenshot_script.ps1
+++ screencrab/src-tauri/src/screenshot_script.ps1
@@ -1,8 +1,23 @@
 Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
+# Make the process DPI aware to ensure the screenshot captures the entire screen
+Add-Type -TypeDefinition @"
+    using System.Runtime.InteropServices;
+
+    public class DPI {
+        [DllImport("user32.dll")]
+        public static extern bool SetProcessDPIAware();
+    }
+"@ 
+
+[DPI]::SetProcessDPIAware()
+
 $Screen = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds
-$Bitmap = New-Object System.Drawing.Bitmap $Screen.Width, $Screen.Height
+$Width = $Screen.Width
+$Height = $Screen.Height
+
+$Bitmap = New-Object System.Drawing.Bitmap $Width, $Height
 $Graphics = [System.Drawing.Graphics]::FromImage($Bitmap)
-$Graphics.CopyFromScreen($Screen.Location, [System.Drawing.Point]::Empty, $Screen.Size)
+$Graphics.CopyFromScreen(0, 0, 0, 0, $Screen.Size)
 $Bitmap.Save('C:\Users\Antonio\Downloads\screenshot.png', [System.Drawing.Imaging.ImageFormat]::Png)
