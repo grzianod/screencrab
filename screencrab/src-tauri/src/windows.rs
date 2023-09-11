bed84e4b284336259c5b8c55c6895441024074de
Graziano Dinocca <grazianodinocca@icloud.com>
2023-09-11 15:29:08 +0200

    windows full working application

--- screencrab/src-tauri/src/windows.rs
+++ screencrab/src-tauri/src/windows.rs
@@ -48,7 +48,7 @@ pub async fn capture_fullscreen(window: Window, filename: &str, file_type: &str,
 
     window.listen_global("kill", move |_event| {
         tokio::task::spawn(async move {
-           Command::new("taskkill").args(&["/F", "/PID", &pid.to_string()]).output().await;
+            Command::new("taskkill").args(&["/F", "/PID", &pid.to_string()]).output().await.unwrap();
         });
     });
 
@@ -101,7 +101,7 @@ pub async fn capture_custom(window: Window, area: &str, filename: &str, file_typ
 
     window.listen_global("kill", move |_event| {
         tokio::task::spawn(async move {
-            Command::new("taskkill").args(&["/F", "/PID", &pid.to_string()]).output().await;
+            Command::new("taskkill").args(&["/F", "/PID", &pid.to_string()]).output().await.unwrap();
         });
     });
 
@@ -118,93 +118,43 @@ pub async fn capture_custom(window: Window, area: &str, filename: &str, file_typ
 
 
 pub async fn record_fullscreen(window: Window, filename: &str, timer: u64, pointer: bool, clipboard: bool, audio: bool, open_file: bool) -> Response {
-    let mut process = Command::new("ffmpeg")
-        .stdin(Stdio::piped())
-        .arg("-y")
-        .args(&["-f", "gdigrab"])
-        .args(&["-i", "desktop"])
-        .arg(&filename)
-        .spawn()
-        .unwrap();
+    const SCRIPT: &[u8] = include_bytes!("record_full_script.ps1");
+    let temp_dir = std::env::temp_dir();
+    let temp_file_path = temp_dir.join("record_full_script.ps1");
 
-    window.menu_handle().get_item("stop_recording").set_enabled(true).unwrap();
-    window.menu_handle().get_item("custom_record").set_enabled(false).unwrap();
-    window.menu_handle().get_item("fullscreen_record").set_enabled(false).unwrap();
-
-    let stdin = process.stdin.take().unwrap();
-    let stdin = Arc::new(Mutex::new(stdin));
-    let pid = process.id().unwrap();
-
-    window.listen_global("kill", move |_event| {
-        tokio::task::spawn(async move {
-            Command::new("taskkill").args(&["/F", "/PID", &pid.to_string()]).output().await;
-        });
-    });
-
-    let window_ = window.clone();
-    let pid2 = process.id().unwrap();
-
-    window.listen_global("stop", move |_event| {
-            unsafe { GenerateConsoleCtrlEvent(CTRL_BREAK_EVENT, pid as u32); }
-        window_.menu_handle().get_item("stop_recording").set_enabled(false).unwrap();
-        window_.menu_handle().get_item("custom_record").set_enabled(true).unwrap();
-        window_.menu_handle().get_item("fullscreen_record").set_enabled(true).unwrap();
-    });
-
-    let output = process.wait().await.unwrap();
-    let filename1 = filename.to_string();
-    let output = process.wait().await.unwrap();
-    if output.success() {
-        if open_file {
-            // Use tokio::task::spawn to execute the opening
-            let _open_task = task::spawn(async move {
-                let _open = Command::new("Start-Process").arg(filename1.to_string()).output().await.map_err(|e| Response::new(None, Some(format!("Failed to open screenshot: {}", e)) ));
-            });
-        }
-        return Response::new(Some(format!("Screen Crab saved to {}", filename.to_string())), None);
+    {
+        let mut temp_file = fs::File::create(&temp_file_path).unwrap();
+        temp_file.write_all(SCRIPT).unwrap();
     }
-    return Response::new(None, Some(format!("Screen Crab cancelled")) );
-}
 
-
-pub async fn record_custom(window: Window, area: &str, filename: &str, timer: u64, _pointer: bool, _clipboard: bool, audio: bool, open_file: bool) -> Response {
-    let parts: Vec<&str> = area.split(',').collect();
-    let x = parts[0].trim().parse::<i32>().unwrap();
-    let y = parts[1].trim().parse::<i32>().unwrap();
-    let width = parts[2].trim().parse::<i32>().unwrap();
-    let height = parts[3].trim().parse::<i32>().unwrap();
-
-    println!("{}, {}, {}, {}", x,y,width, height);
-
-    let mut process = Command::new("ffmpeg")
-        .stdin(Stdio::piped())
-        .arg("-y")
-        .args(&["-f", "gdigrab"])
-        .args(&["-video_size", &format!("{}x{}", width, height)])
-        .args(&["-offset_x", &x.to_string()])
-        .args(&["-offset_y", &y.to_string()])
-        .args(&["-i", "desktop"])
-        .arg(&filename)
+    let process = Command::new("powershell")
+        .creation_flags(0x00000200)
+        .arg("-ExecutionPolicy")
+        .arg("Bypass")
+        .arg("-File")
+        .arg(temp_file_path.clone())  // Name of the screen recording script
+        .arg("-filename")
+        .arg(filename)
+        .arg("-timer")
+        .arg(timer.to_string())  // Convert u64 to String
+        .arg("-audio")
+        .arg(if audio { "True" } else { "False" })  // Convert to "1" or "0"
         .spawn()
-        .unwrap();
+        .map_err(|e| Response::new( None, Some(format!("Failed to take screenshot: {}", e)) ));
 
     window.menu_handle().get_item("stop_recording").set_enabled(true).unwrap();
     window.menu_handle().get_item("custom_record").set_enabled(false).unwrap();
     window.menu_handle().get_item("fullscreen_record").set_enabled(false).unwrap();
 
-    let stdin = process.stdin.take().unwrap();
-    let stdin = Arc::new(Mutex::new(stdin));
-    let pid = process.id().unwrap();
+    let pid = process.as_ref().unwrap().id().unwrap();
 
     window.listen_global("kill", move |_event| {
         tokio::task::spawn(async move {
-            Command::new("taskkill").args(&["/F", "/PID", &pid.to_string()]).output().await;
+            Command::new("taskkill").arg("/F").arg("/PID").arg(&pid.to_string()).output().await.unwrap();
         });
     });
 
     let window_ = window.clone();
-    let pid2 = process.id().unwrap();
-
     window.listen_global("stop", move |_event| {
         unsafe { GenerateConsoleCtrlEvent(CTRL_BREAK_EVENT, pid as u32); }
         window_.menu_handle().get_item("stop_recording").set_enabled(false).unwrap();
@@ -212,14 +162,78 @@ pub async fn record_custom(window: Window, area: &str, filename: &str, timer: u6
         window_.menu_handle().get_item("fullscreen_record").set_enabled(true).unwrap();
     });
 
-    let output = process.wait().await.unwrap();
+    let output = process.unwrap().wait().await.unwrap();
+    fs::remove_file(&temp_file_path).unwrap();
     let filename1 = filename.to_string();
-    let output = process.wait().await.unwrap();
     if output.success() {
         if open_file {
             // Use tokio::task::spawn to execute the opening
             let _open_task = task::spawn(async move {
-                let _open = Command::new("Start-Process").arg(filename1.to_string()).output().await.map_err(|e| Response::new(None, Some(format!("Failed to open screenshot: {}", e)) ));
+                let _open = Command::new("cmd").arg("/C").arg(filename1.as_str()).output().await.map_err(|e| Response::new(None, Some(format!("Failed to open screenshot: {}", e)) ));
+            });
+        }
+        return Response::new(Some(format!("Screen Crab saved to {}", filename.to_string())), None);
+    }
+    return Response::new(None, Some(format!("Screen Crab cancelled")) );
+
+}
+
+
+pub async fn record_custom(window: Window, area: &str, filename: &str, timer: u64, _pointer: bool, _clipboard: bool, audio: bool, open_file: bool) -> Response {
+    const SCRIPT: &[u8] = include_bytes!("record_custom_script.ps1");
+    let temp_dir = std::env::temp_dir();
+    let temp_file_path = temp_dir.join("record_custom_script.ps1");
+
+    {
+        let mut temp_file = fs::File::create(&temp_file_path).unwrap();
+        temp_file.write_all(SCRIPT).unwrap();
+    }
+
+    let process = Command::new("powershell")
+        .creation_flags(0x00000200)
+        .arg("-ExecutionPolicy")
+        .arg("Bypass")
+        .arg("-File")
+        .arg(temp_file_path.clone())  // Name of the screen recording script
+        .arg("-filename")
+        .arg(filename)
+        .arg("-timer")
+        .arg(timer.to_string())  // Convert u64 to String
+        .arg("-area")
+        .arg(&area.to_string())
+        .arg("-audio")
+        .arg(if audio { "True" } else { "False" })  // Convert to "1" or "0"
+        .spawn()
+        .map_err(|e| Response::new( None, Some(format!("Failed to take screenshot: {}", e)) ));
+
+    window.menu_handle().get_item("stop_recording").set_enabled(true).unwrap();
+    window.menu_handle().get_item("custom_record").set_enabled(false).unwrap();
+    window.menu_handle().get_item("fullscreen_record").set_enabled(false).unwrap();
+
+    let pid = process.as_ref().unwrap().id().unwrap();
+
+    window.listen_global("kill", move |_event| {
+        tokio::task::spawn(async move {
+            let _ = Command::new("taskkill").arg("/F").arg("/PID").arg(&pid.to_string()).output().await.unwrap();
+        });
+    });
+
+    let window_ = window.clone();
+    window.listen_global("stop", move |_event| {
+        unsafe { GenerateConsoleCtrlEvent(CTRL_BREAK_EVENT, pid as u32); }
+        window_.menu_handle().get_item("stop_recording").set_enabled(false).unwrap();
+        window_.menu_handle().get_item("custom_record").set_enabled(true).unwrap();
+        window_.menu_handle().get_item("fullscreen_record").set_enabled(true).unwrap();
+    });
+
+    let output = process.unwrap().wait().await.unwrap();
+    fs::remove_file(&temp_file_path).unwrap();
+    let filename1 = filename.to_string();
+    if output.success() {
+        if open_file {
+            // Use tokio::task::spawn to execute the opening
+            let _open_task = task::spawn(async move {
+                let _open = Command::new("cmd").arg("/C").arg(filename1.as_str()).output().await.map_err(|e| Response::new(None, Some(format!("Failed to open screenshot: {}", e)) ));
             });
         }
         return Response::new(Some(format!("Screen Crab saved to {}", filename.to_string())), None);
