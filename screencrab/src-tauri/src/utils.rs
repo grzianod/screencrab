78372be10b5f952d0adf5027848c0be5093995d2
Graziano Dinocca <grazianodinocca@icloud.com>
2023-11-26 15:08:19 +0100

    linux&windows clipboard functionality

--- screencrab/src-tauri/src/utils.rs
+++ screencrab/src-tauri/src/utils.rs
@@ -204,7 +204,7 @@ pub fn copy_to_clipboard(path: String) -> Response {
     let mut image_bytes = Vec::new();
     image.write_to(&mut Cursor::new(&mut image_bytes), format).unwrap();
 
-    if let Err(err) = ctx.set_contents(String::from_utf8(image_bytes).unwrap()) {
+    if let Err(err) = ctx.set_contents(String::from_utf8_unchecked(image_bytes)) {
         return Response::new(None, Some(err.to_string()));
     }
     else {
